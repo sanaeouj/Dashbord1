617064e483893c36927f6ad38efeb1293d3a9b4b
sanaeouj <sanae.ouajhain@gmail.com>
2024-11-17 15:23:05 +0100

    Add Navbar

---
+++ Navbar.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+import { styled, alpha, useTheme } from "@mui/material/styles";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import InputBase from "@mui/material/InputBase";
+import Stack from "@mui/material/Stack";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import Box from "@mui/material/Box";
+import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+
+// Largeur du drawer
+const drawerWidth = 240;
+
+// Styled AppBar avec transition basée sur la prop `open`
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+// Composant stylisé pour la barre de recherche
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+// Wrapper pour l'icône de recherche
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+// Input stylisé à l'intérieur de la barre de recherche
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
+// Composant Navbar
+const Navbar = ({ open, handleDrawerOpen, toggleMode }) => {
+  const theme = useTheme();
+
+  return (
+    <AppBar position="fixed" open={open}>
+      <Toolbar>
+        {/* Bouton pour ouvrir le Drawer */}
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          sx={{ marginRight: 5, ...(open && { display: "none" }) }}
+        >
+          <MenuIcon />
+        </IconButton>
+
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ "aria-label": "search" }}
+          />
+        </Search>
+
+        <Box flexGrow={1} />
+        <Stack direction="row" spacing={2}>
+          {/* Bouton pour basculer entre clair/sombre */}
+          <IconButton onClick={toggleMode} color="inherit">
+            {theme.palette.mode === " light" ? (
+              <LightModeOutlinedIcon />
+            ) : (
+              <DarkModeOutlinedIcon />
+            )}
+          </IconButton>
+
+          {/* Icônes de navigation */}
+          <IconButton color="inherit">
+            <SettingsOutlinedIcon />
+          </IconButton>
+          <IconButton color="inherit">
+            <NotificationsNoneOutlinedIcon />
+          </IconButton>
+          <IconButton color="inherit">
+            <Person2OutlinedIcon />
+          </IconButton>
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar; 
